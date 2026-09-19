1179792ba97efdd1385f681c18f8626179392c41
ChoDoGyu <doing9498@gmail.com>
2026-09-19 11:51:34 +0900

    chore: Map Generation 패키지 기본 구조 구성

---
+++ com.chodogyu.map-generation/Runtime/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("CDG.MapGeneration.Editor")]
+[assembly: InternalsVisibleTo("CDG.MapGeneration.Tests.Runtime")]
+[assembly: InternalsVisibleTo("CDG.MapGeneration.Tests.Editor")]
